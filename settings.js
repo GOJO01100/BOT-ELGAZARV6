b3b452432bfa6595187ff921e2d180e76f1ca6af
GOJO01100 <136199644+GOJO01100@users.noreply.github.com>
2023-06-11 00:18:31 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201100829817'] //ur owner number
+global.ownername = "GOJO" //ur owner name
+global.ytname = "YT: GOJO" //ur yt chanel name
+global.socialm = "GitHub: GOJO" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "201100829817" //ur number
+global.premium = ['201100829817'] //ur premium number
+global.botname = 'GOJO' //ur bot name
+global.linkz = "https://www.youtube.com/@appYORKIE" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://www.youtube.com/@appYORKIE' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "غوجو ساتورو عم الجميع" //ur sticker watermark packname
+global.author = "01100829817" //ur sticker watermark author
+global.wm = "GOJO" //ur watermark
 
 // Other
 global.sessionName = 'session'
